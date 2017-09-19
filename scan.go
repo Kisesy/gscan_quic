cefe455a3657f31bb5aa2c9f777d9a37762503e1
kisesy <kisesy@gmail.com>
2017-09-19 12:03:52 +0800

    添加 G3 ECC 证书验证

--- scan.go
+++ scan.go
@@ -135,6 +135,7 @@ var (
 	}
 	g2pkp, _ = base64.StdEncoding.DecodeString("7HIpactkIAq2Y49orFOOQKurWxmmSFZhBCoQYcRhJ3Y=")
 	g3pkp, _ = base64.StdEncoding.DecodeString("f8NnEFZxQ4ExFOhSN7EiFWtiudZQVD2oY60uauV/n78=")
+	g3ecc, _ = base64.StdEncoding.DecodeString("ekG8/PoSqjfKunOaS9iIzR3hZAJptWJKV7CgyriO+MA=")
 )
 
 func testip_once(ip string, options *ScanOptions, record *ScanRecord) bool {
@@ -214,7 +215,7 @@ func testip_once(ip string, options *ScanOptions, record *ScanRecord) bool {
 			return
 		}
 		pkp := sha256.Sum256(pcs[1].RawSubjectPublicKeyInfo)
-		if !bytes.Equal(g2pkp, pkp[:]) && !bytes.Equal(g3pkp, pkp[:]) {
+		if !bytes.Equal(g2pkp, pkp[:]) && !bytes.Equal(g3pkp, pkp[:]) && !bytes.Equal(g3ecc, pkp[:]) {
 			success <- false
 			return
 		}
